4d0234052d3c23368a123c0762d200038fbcfd42
flagarde <flagarde.programs@pm.me>
2022-05-24 02:17:58 +0800

    feat: Basic files

---
+++ cmake/GetCMakeMM.cmake
@@ -0,0 +1,11 @@
+# cmake-format: off
+if(GETCMMM_INCLUDED)
+  return()
+endif()
+set(GETCMMM_INCLUDED TRUE)
+
+# cmake-lint: disable=C0103
+set(GET_CMMM_VERSION "1.0.0" CACHE INTERNAL "Loaded version of GetCMakeMM")
+# pass
+
+# cmake-format: on

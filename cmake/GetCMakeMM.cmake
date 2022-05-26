95643d7364d90908d58acdcf4c2d90a733f4f404
flagarde <flagarde.programs@pm.me>
2022-05-27 00:46:22 +0800

    test: Try color

--- cmake/GetCMakeMM.cmake
+++ cmake/GetCMakeMM.cmake
@@ -4,10 +4,20 @@ if(GETCMMM_INCLUDED)
 endif()
 set(GETCMMM_INCLUDED TRUE)
 
+if(CMAKE_VERSION VERSION_GREATER 3.1)
+  set(CMMM_ECHO "echo")
+  set(CMMM_RED "")
+  set(CMMM_MAGENTA "")
+else()
+  set(CMMM_ECHO "cmake_echo_color")
+  set(CMMM_RED "--red")
+  set(CMMM_MAGENTA "--purple")
+endif()
+
 # cmake-lint: disable=C0103
 set(GET_CMMM_VERSION "1.0.0" CACHE INTERNAL "Loaded version of GetCMakeMM")
 # pass
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E env CLICOLOR_FORCE=1 ${CMAKE_COMMAND} -E cmake_echo_color --no-newline --red toto)
+execute_process(COMMAND ${CMAKE_COMMAND} -E env CLICOLOR_FORCE=1 ${CMAKE_COMMAND} -E ${CMMM_ECHO} ${CMMM_RED} toto)
 
 # cmake-format: on

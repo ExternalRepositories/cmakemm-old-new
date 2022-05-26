c6560e1b20b9502c35adc82f8d510680f4c836ec
flagarde <flagarde.programs@pm.me>
2022-05-27 01:38:58 +0800

    ci: Test color

--- cmake/GetCMakeMM.cmake
+++ cmake/GetCMakeMM.cmake
@@ -5,10 +5,12 @@ endif()
 set(GETCMMM_INCLUDED TRUE)
 
 if(CMAKE_VERSION VERSION_LESS 3.1)
+	set(CMMM_ENV "")
   set(CMMM_ECHO "echo")
   set(CMMM_RED "")
   set(CMMM_MAGENTA "")
 else()
+	set(CMMM_ENV "${CMAKE_COMMAND} -E env CLICOLOR_FORCE=1")
   set(CMMM_ECHO "cmake_echo_color")
   set(CMMM_RED "--red")
   set(CMMM_MAGENTA "--purple")
@@ -18,6 +20,6 @@ endif()
 set(GET_CMMM_VERSION "1.0.0" CACHE INTERNAL "Loaded version of GetCMakeMM")
 # pass
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E ${CMMM_ECHO} ${CMMM_RED} toto)
+execute_process(COMMAND ${CMAKE_COMMAND} -E env CLICOLOR_FORCE=1 ${CMAKE_COMMAND} -E ${CMMM_ECHO} ${CMMM_RED} toto)
 
 # cmake-format: on
